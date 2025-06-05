12f0bd537c2d0afcaa0e7c17636c456fdfb9fd02
Andreas <andreas.vralstad@gmail.com>
2025-06-05 19:22:24 +0200

    Removed spaces

--- main.cpp
+++ main.cpp
@@ -670,14 +670,11 @@ int main() {
 	glGenVertexArrays(1, &buffer_info_background.vao);
 	glGenBuffers(1, &buffer_info_background.vbo);
 	glGenBuffers(1, &buffer_info_background.ebo);
-
 	glBindVertexArray(buffer_info_background.vao);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer_info_background.vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(bgVertices), bgVertices, GL_STATIC_DRAW);
-
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer_info_background.ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(bgIndices), bgIndices, GL_STATIC_DRAW);
-
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
